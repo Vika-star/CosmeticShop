9b3106b9c2217f442526918c2540962e878201fe
Vika-star <72336972+Vika-star@users.noreply.github.com>
2021-06-12 11:06:54 +0300

    ctop initialize productContainer in order product accounting

--- CosmeticShop/CosmeticShop/Models/Products/OrderProductAccounting.cs
+++ CosmeticShop/CosmeticShop/Models/Products/OrderProductAccounting.cs
@@ -9,7 +9,7 @@ namespace CosmeticShop.Models.Products
     public class OrderProductAccounting
     {
         public int Id { get; set; }
-        public OrderProductAccounting() => ProductContainer = new();
+        //public OrderProductAccounting() => ProductContainer = new();
         public int CountRequiredProducts { get; set; }
 
         [NotMapped]
